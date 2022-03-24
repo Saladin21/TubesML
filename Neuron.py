eed9e11b4099fd9dabc240f24089550b90dae117
Purecon <mfurqon2001@gmail.com>
2022-03-24 08:31:22 +0700

    Add neuron calculate error function

--- Neuron.py
+++ Neuron.py
@@ -1,3 +1,6 @@
+from Neuron import Activation
+import derived_activation_function as derived
+
 class Neuron:
     bobot = None
     netValue = None
@@ -5,6 +8,8 @@ class Neuron:
     def __init__(self, bobot):
         self.bobot = bobot
         self.netValue = None
+        self.errorFactor = None
+        self.deltaWeight = None
     # fungsi hitungValue menghitung net dari penjumlahan antara perkalian bobot dengan input
     def hitungValue(self,input):
         self.netValue = sum(x * y for x, y in zip(self.bobot, input))
@@ -13,8 +18,32 @@ class Neuron:
     def printNeuron(self):
         print(self.bobot)
     
-
-        
+    #Hitung error output
+    #NOTES
+    #Output ada di layer?
+    #Target belum ada
+    #Fungsi turunan dari output
+    def calculateErrorOut(self,output,activation,target):
+        #NOTES
+        #-log(pk)
+        #ini masukin errorFactor aja?
+        if(activation == Activation.softmax):
+            return (-1/target)
+        else:
+            return (output*(1-output)*(target-output))
+   
+    #Hitung hidden error
+    #Hitung gradient 
+    #X merupakan input
+    def calculateHiddenError(self,output,activation,target,x):
+        if(activation == Activation.softmax):
+            self.errorFactor = derived.derived_softmax(output,target)
+        elif(activation == Activation.linear):
+            self.errorFactor = -(target-output)* derived.derived_sigmoid(output) * x 
+        elif(activation == Activation.RELU):
+            self.errorFactor = -(target-output)* derived.derived_RELU(output) * x
+        elif(activation == Activation.sigmoid):
+            self.errorFactor = -(target-output)* derived.derived_linear(output) * x
 
 # p1 = Neuron()
 # p1.hitungValue([1,2,2],[1,3,5])
